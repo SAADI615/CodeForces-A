62de52c61bc8024c01abedc1abc1a87904442a65
Salim Sadman <96647391+SAADI615@users.noreply.github.com>
2024-11-19 17:48:33 +0600

    Create A_Second_Order_Statistics.cpp

---
+++ A_Second_Order_Statistics.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    set<int> s;
+    for(int i=0; i<n; i++){
+        int a;
+        cin>>a;
+        s.insert(a);
+    }
+    if(s.size()>1){
+        cout<<*(++s.begin())<<endl;
+    }
+    else cout<<"NO"<<endl;
+}
